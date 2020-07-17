7ad0df33a29dac084878226eed2278ae2cf29c3f
takuizum <40698749+takuizum@users.noreply.github.com>
2020-07-18 02:08:50 +0900

    Update Equate doc

--- src/Equate.jl
+++ src/Equate.jl
@@ -53,7 +53,8 @@ export
 abstract type EquateDesign end
 """
     EG
-Equivalent (single) group design.
+Equivalent (single) group design. 
+If the population of 2 tests is identical, the test design will be called the single group design.
 """
 abstract type EG <: EquateDesign end
 """
@@ -81,7 +82,23 @@ Major features:
 - `Linear` provides the linear equating.
 - `Equipercentile` provides the equipercentile equating.
 
+*Non Equivalent group with Anchor Test (NEAT) design*
+
+- `Tucker`
+- `ChainedLinear`
+- `ChainedEquipercentile`
+- `BraunHolland`
+- `FrequencyEstimation`
+
 and more...
+
+According to von Davier, Holland, and Thayer (2004), the test equating has separate 5 steps.
+
+- First step, *Pre-smoothing*.
+- Second step is *Estimating the score probabilities*.
+- Third step is *Continuization*.
+- Fourth step is *Equating*.
+- Fifth, last step is *Calculating the Standard Error of Equating(SEE)*.
 
 """
 Equate
