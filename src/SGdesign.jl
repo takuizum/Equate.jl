fab5f6bf985c05103fe8ba12ba2427fd38e513dc
takuizum <40698749+takuizum@users.noreply.github.com>
2020-08-24 14:31:09 +0900

    Up doc

--- src/SGdesign.jl
+++ src/SGdesign.jl
@@ -52,6 +52,8 @@ end
     Equipercentile(X::EG, Y::EG; case = :middle)
 
 Equipercentile Equating under equivalent (single) group design. The smoothed frequency can be used.
+A table returned by the function contains 2 columns. The first one stands for the base scale score in the test X.
+The second one is equated score from test Y, which is equipercentile score on the base scale.
 
 `case` represents which equating case use. Pass to the symbols below.
 
