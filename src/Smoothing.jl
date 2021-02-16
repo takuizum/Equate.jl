a14c5bb3fddf2c809c053ad6b872c35945d9cb8c
takuizum <sep10.taku.izum@gmail.com>
2021-02-16 21:42:37 +0900

    rm @simd

--- src/Smoothing.jl
+++ src/Smoothing.jl
@@ -6,11 +6,10 @@
 Returns GLM formula can be used in `glm` with `FreqTab` data frame.
 `df`, is abbreviation for degrees of formula, represents degree of polynomial log-linear method.
 This function works very slowly. If a fixed degree formula will be used repeatedly, define the formula as a variable.
-The similer macro `@LogLinearFormula` is provided and it is faster than the function.
 """
 function LogLinearFormula(df)
     fml = "@formula(freq ~ 1 +"
-    @simd for d in 1:df
+    for d in 1:df
         fml *= "scale^$d"
         if d != df
             fml *= " + "
