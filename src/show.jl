d37d1e022f2bf6b88b31a1c1ff7efb231e856b7b
takuizum <sep10.taku.izum@gmail.com>
2021-05-09 02:46:36 +0900

    Improve show method

--- src/show.jl
+++ src/show.jl
@@ -14,40 +14,44 @@ julia> julia> Equate.minishow.(keys(A), values(A))
 ```
 """
 
-function minishow(k, v)
+function minishow(io::IO, k, v)
     if v isa Float64
-        @printf "%10s : %3.5f \n" k v
+        @printf io "%10s : %3.5f \n" k v
     elseif v isa Int64
-        @printf "%10s : %8i \n" k v
+        @printf io "%10s : %8i \n" k v
     elseif v isa String
-        @printf "%10s : %10s \n" k v
+        @printf io "%10s : %10s \n" k v
     else
-        @printf "%10s : can't be shown! \n" k
+        @printf io "%10s : can't be shown! \n" k
     end
 end
 
-function minishow(A)
-    minishow.(keys(A), values(A))
+function minishow(io::IO, A)
+    minishow.(Ref(io), keys(A), values(A))
 end
 
 function Base.show(io::IO, x::FreqTab)
-    println("Frequency table stats.")
-    minishow(x.stats)
+    println(io, "Frequency table stats.")
+    minishow(io, x.stats)
+    return
 end
 
 function Base.show(io::IO, x::NEATFreqTab)
-    println("Frequency table stats.")
-    println("* X (independent part)")
-    minishow(x.statsX)
-    println("* V (common part)")
-    minishow(x.statsV)
+    println(io, "Frequency table stats.")
+    println(io, "* X (independent part)")
+    minishow(io, x.statsX)
+    println(io, "* V (common part)")
+    minishow(io, x.statsV)
+    return
 end
 
 function Base.show(io::IO, x::NEATEquateResult)
-    println("Equating design: NEAT\nEquated method: $(x.method).\nTo show the table, extract `tbl` element.")
-    println(x.synthetic)
+    println(io, "Equating design: NEAT\nEquated method: $(x.method).\nTo show the table, extract `tbl` element.")
+    println(io, x.synthetic)
+    return
 end
 
 function Base.show(io::IO, x::SGEquateResult)
-    println("Equating design: EG\nEquated method: $(x.method).\nTo show the table, extract `tbl` element.")
+    println(io, "Equating design: EG\nEquated method: $(x.method).\nTo show the table, extract `tbl` element.")
+    return
 end
